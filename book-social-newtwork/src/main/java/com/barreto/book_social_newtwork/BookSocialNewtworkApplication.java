73fe4e1fd1702ebd65f09786622199573ddfbdba
Pichau <igorof06@outlook.com>
2024-10-07 20:16:57 -0300

    feat: starting project

---
+++ book-social-newtwork/src/main/java/com/barreto/book_social_newtwork/BookSocialNewtworkApplication.java
@@ -0,0 +1,13 @@
+package com.barreto.book_social_newtwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNewtworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNewtworkApplication.class, args);
+	}
+
+}
---
+++ book-social-newtwork/src/test/java/com/barreto/book_social_newtwork/BookSocialNewtworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barreto.book_social_newtwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNewtworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
